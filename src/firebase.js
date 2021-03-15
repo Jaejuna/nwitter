d550a3caa8b9d9e314fb3b064f77c17badd13e61
Jaejuna <anayana9988@gmail.com>
2021-03-16 03:40:53 +0900

    directory moved

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABouPV04qUL-LNpOiNNi8WwpdDCWmEvTM",
+  authDomain: "nwitter-8b1dd.firebaseapp.com",
+  projectId: "nwitter-8b1dd",
+  storageBucket: "nwitter-8b1dd.appspot.com",
+  messagingSenderId: "196690237257",
+  appId: "1:196690237257:web:145b167a5178f77968ac98"
+};
+
+export default firebase.initializeApp(firebaseConfig);
