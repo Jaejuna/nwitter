0ed52560639b48a5c31be4c86d9401e94ce4a4ac
Jaejuna <anayana9988@gmail.com>
2021-03-17 23:09:11 +0900

    1.5 done with uploading

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,19 @@ import AppRouter from './router';
 
 function App() {
   const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userObj, setUserObj] = useState(null);
   useEffect(() => {
     authService.onAuthStateChanged((user) => {
       if (user) {
-        setIsLoggedIn(true);
         setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
-      }
+      } 
       setInit(true);
     });
   }, [])
   return (
     <>
-      {init ? (<AppRouter isLoggedIn={isLoggedIn} userObj={userObj}/>
+      {init ? (
+      <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
       ) : (
         "Initializing..."
       )}
