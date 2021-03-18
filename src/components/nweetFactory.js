9c39c3e26b21533f03575f6bc8ce55d1dde533e9
Jaejuna <anayana9988@gmail.com>
2021-03-19 01:48:01 +0900

    1.9.1 css done

--- src/components/nweetFactory.js
+++ src/components/nweetFactory.js
@@ -1,10 +1,10 @@
-    import React, { useState } from "react";
-    import { v4 as uuidv4 } from "uuid";
-    import { storageService, dbService } from "../firebase";
-    import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-    import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
+import React, {useState} from "react";
+import {v4 as uuidv4} from "uuid";
+import {storageService, dbService} from "../firebase";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faPlus, faTimes} from "@fortawesome/free-solid-svg-icons";
 
-    const NweetFactory = ({ userObj }) => {
+const NweetFactory = ({userObj}) => {
     const [nweet, setNweet] = useState("");
     const [attachment, setAttachment] = useState("");
     const onSubmit = async (event) => {
@@ -14,39 +14,43 @@
         event.preventDefault();
         let attachmentUrl = "";
         if (attachment !== "") {
-        const attachmentRef = storageService
-            .ref()
-            .child(`${userObj.uid}/${uuidv4()}`);
-        const response = await attachmentRef.putString(attachment, "data_url");
-        attachmentUrl = await response.ref.getDownloadURL();
+            const attachmentRef = storageService
+                .ref()
+                .child(`${userObj.uid}/${uuidv4()}`);
+            const response = await attachmentRef.putString(attachment, "data_url");
+            attachmentUrl = await response
+                .ref
+                .getDownloadURL();
         }
         const nweetObj = {
-        text: nweet,
-        createdAt: Date.now(),
-        creatorId: userObj.uid,
-        attachmentUrl,
+            text: nweet,
+            createdAt: Date.now(),
+            creatorId: userObj.uid,
+            attachmentUrl
         };
-        await dbService.collection("nweets").add(nweetObj);
+        await dbService
+            .collection("nweets")
+            .add(nweetObj);
         setNweet("");
         setAttachment("");
     };
     const onChange = (event) => {
-        const {
-        target: { value },
-        } = event;
+        const {target: {
+                value
+            }} = event;
         setNweet(value);
     };
     const onFileChange = (event) => {
-        const {
-        target: { files },
-        } = event;
+        const {target: {
+                files
+            }} = event;
         const theFile = files[0];
         const reader = new FileReader();
         reader.onloadend = (finishedEvent) => {
-        const {
-            currentTarget: { result },
-        } = finishedEvent;
-        setAttachment(result);
+            const {currentTarget: {
+                    result
+                }} = finishedEvent;
+            setAttachment(result);
         };
         reader.readAsDataURL(theFile);
     };
@@ -60,15 +64,12 @@
                     onChange={onChange}
                     type="text"
                     placeholder="What's on your mind?"
-                    maxLength={120}
-                />
-                <input type="submit" 
-                value="&rarr;" 
-                className="factoryInput__arrow" />
+                    maxLength={120}/>
+                <input type="submit" value="&rarr;" className="factoryInput__arrow"/>
             </div>
             <label for="attach-file" className="factoryInput__label">
                 <span>Add photos</span>
-                <FontAwesomeIcon icon={faPlus} />
+                <FontAwesomeIcon icon={faPlus}/>
             </label>
             <input
                 id="attach-file"
@@ -76,25 +77,24 @@
                 accept="image/*"
                 onChange={onFileChange}
                 style={{
-                    opacity: 0,
-                }}
-            />
-
-            {attachment && (
-                <div className="factoryForm__attachment">
-                    <img
-                        src={attachment}
-                        style={{
-                            backgroundImage: attachment,
-                        }}
-                    />
-                    <div className="factoryForm__clear" onClick={onClearAttachment}>
-                        <span>Remove</span>
-                        <FontAwesomeIcon icon={faTimes} />
+                    opacity: 0
+                }}/> {
+                attachment && (
+                    <div className="factoryForm__attachment">
+                        <img
+                            src={attachment}
+                            style={{
+                                backgroundImage: attachment
+                            }}
+                        />
+                        <div className="factoryForm__clear" onClick={onClearAttachment}>
+                            <span>Remove</span>
+                            <FontAwesomeIcon icon={faTimes}/>
+                        </div>
                     </div>
-                </div>
-            )}
+                )
+            }
         </form>
     );
-    };
-    export default NweetFactory;
+};
+export default NweetFactory;
